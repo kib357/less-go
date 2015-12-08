5265eccdfee41cfd4c8f5b61911268cebddf7c2f
ivahaev <ivahaev@gmail.com>
2015-12-08 15:44:29 +0500

    CLI and API

---
+++ less.go
@@ -0,0 +1,137 @@
+package less
+
+import (
+	"encoding/json"
+	"errors"
+	"gopkg.in/olebedev/go-duktape.v2"
+	"io/ioutil"
+	"os"
+)
+
+var (
+	ctx *duktape.Context
+	r   = reader{}
+	w   = writer{}
+)
+
+type Reader interface {
+	ReadFile(string) ([]byte, error)
+}
+
+type Writer interface {
+	WriteFile(string, []byte, os.FileMode) error
+}
+
+type reader struct{}
+
+func (reader) ReadFile(path string) ([]byte, error) {
+	return ioutil.ReadFile(path)
+}
+
+type writer struct{}
+
+func (writer) WriteFile(path string, data []byte, mode os.FileMode) error {
+	return ioutil.WriteFile(path, data, mode)
+}
+
+func readFile(c *duktape.Context) int {
+	var path = c.SafeToString(-1)
+	if path == "" {
+		return 0
+	}
+	bytes, err := r.ReadFile(path)
+	if err != nil {
+		bytes, err = Asset(path)
+		if err != nil {
+			return 0
+		}
+	}
+	c.PushString(string(bytes))
+	return 1
+}
+
+func writeFile(c *duktape.Context) int {
+	var data = []byte(c.SafeToString(-1))
+	var path = c.SafeToString(-2)
+	if path == "" {
+		return 0
+	}
+	err := w.WriteFile(path, data, 0644)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
+
+func SetReader(customReader Reader) {
+	r = customReader.(reader)
+}
+
+func SetWriter(customWriter Writer) {
+	w = customWriter.(writer)
+}
+
+func RenderFile(input, output string, mods ...map[string]interface{}) error {
+	if input == "" {
+		return errors.New("No input path provided")
+	}
+	if output == "" {
+		output = input + ".css"
+	}
+	var options = map[string]interface{}{}
+	if len(mods) > 0 {
+		options = mods[0]
+	}
+	options["filename"] = input
+	encodedOptions, err := json.Marshal(options)
+	if err != nil {
+		return err
+	}
+	ctx.EvalString(`
+		try {
+			var fs = require('./assets/less-go/fs');
+			var less = require('./assets/less-go');
+
+			var data = fs.readFileSync("` + input + `");
+			print('Rendering less: ', data.slice(0,50) + '...');
+			less.render(data, ` + string(encodedOptions) + `, function (e, output) {
+				if (e == null) {
+					print("Rendered");
+					writeFile("` + output + `", output.css);
+				} else {
+					print('Render error', e.stack);
+				}
+			});
+		} catch (e) {
+			print("ERROR!", e.stack);
+		}
+	`)
+
+	return nil
+}
+
+func init() {
+	ctx = duktape.New()
+	ctx.PushGlobalGoFunction("readFile", readFile)
+	ctx.PushGlobalGoFunction("writeFile", writeFile)
+
+	ctx.EvalString(`
+		Duktape.modSearch = function (id, require, exports, module) {
+			id = id.replace(/\.js$/, "");
+			var res = readFile(id + ".js");
+			if (typeof res === 'string') {
+				return res;
+			}
+
+			var res = readFile(id + "/index.js");
+			if (typeof res === 'string') {
+				return 'module.exports = require("' + id + '/index.js")';
+			}
+		    throw new Error('module not found: ' + id);
+		};
+	`)
+
+	// result := ctx.GetString(-1)
+	// ctx.Pop()
+	// fmt.Println("result is:", result)
+}
---
+++ lessc/lessc.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/kib357/less-go"
+	"github.com/spf13/cobra"
+	"fmt"
+)
+
+func main() {
+	var input, output *string
+	rootCmd := &cobra.Command{
+		Use:   "lessc",
+		Short: "LESS CSS compiler",
+		Long:  "Crossplatform LESS CSS compiler with no dependencies",
+		Run: func(cmd *cobra.Command, args []string) {
+			render(*input, *output)
+		},
+	}
+	input = rootCmd.PersistentFlags().StringP("input", "i", "styles.less", "Input less file")
+	output = rootCmd.PersistentFlags().StringP("iutput", "o", "styles.css", "Output less file")
+
+	rootCmd.Execute()
+}
+
+func render(input, output string) {
+	fmt.Println(input, output)
+	less.RenderFile(input, output)
+}
